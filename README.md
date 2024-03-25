fbd5e3e9ac5c615646a1cad4f3f7f4027babdc82
Maciek Czarnik <czajnikowski@gmail.com>
2024-03-25 18:36:59 +0100

    API is like a joke - if you have to explain it - it's not that good 🤪

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# GlassEffect
+# SegmentationEffect
 
-This effect allows you to transform the geometry of your regular, rectangular SwiftUI `View`s into quadrilateral segments ⏢.
+This effect allows you to transform the geometry of your regular, rectangular SwiftUI `View`s into quadrilateral segments ⏢, so you can do fun things like:
 
-The shader uses bilinear interpolation to map pixel positions to the output via `View.distortionEffect`.
+![output](https://github.com/Czajnikowski/SegmentationEffect/assets/973682/58ee8392-45c1-43a1-b655-c9d81764a50a)
 
-![output]()
+The shader uses bilinear interpolation to map pixel positions to the output via `View.distortionEffect`. The repository features an editor app that allows you to give it a try in an interactive way.
 
 ## How To Use It?
 
 Use as a Swift Package.
 
-Use a [`View.geometryEffect` modifier](). At a minimum, you should be able to run the effect by supplying a normal map texture image. For best results, be sure to use high-quality normal maps.
+Use a [`View.segmentationEffect`](https://github.com/Czajnikowski/SegmentationEffect/blob/main/Sources/SegmentationEffect/SegmentationEffect.swift#L5-L20) modifier.
 
 ## Why?
 
-I made it mostly for myself as an exercise in my recent `SwiftUI` + `Metal` research. Its an early version of the package. At this point the APIs has not been crafted very carefully and you can ...
+I made it mostly for myself as an exercise in my recent `SwiftUI` + `Metal` research. At this point APIs have not been crafted very carefully, so the possibilities of the package are quite limited, but give it a try, and be sure to let me know if you like it. I can definitely spend some more time on it, if it is of use for anybody in a real project. At this point - it simply "works for me™️" ⏸️😁
 
 Feel free to use it, feel free to contribute (fix issues, share ideas), and feel free to hit me up [@czajnikowski](https://twitter.com/czajnikowski) 👋
--- Sources/SegmentationEffect/SegmentationEffect.swift
+++ Sources/SegmentationEffect/SegmentationEffect.swift
@@ -1,35 +1,18 @@
 import SwiftUI
 
-public struct Segment {
-  public var bottomEdge: Edge
-  
-  public var contentScale: Float
-  
-  public init(bottomEdge: Edge, contentScale: Float) {
-    self.bottomEdge = bottomEdge
-    self.contentScale = contentScale
-  }
-  
-  var floatArray: [Float] {
-    bottomEdge.floatArray + [contentScale]
-  }
-}
-
-public enum Edge {
-  case node(SIMD2<Float>)
-  case bar(SIMD2<Float>, SIMD2<Float>)
-  
-  var floatArray: [Float] {
-    switch self {
-    case .node(let position):
-      return position.xy + position.xy
-    case .bar(let lPosition, let rPosition):
-      return lPosition.xy + rPosition.xy
-    }
-  }
-}
-
 extension View {
+  /// Applies an effect that renders the content of the modified `View` as a top-to-bottom series of quadrilateral segments.
+  ///
+  /// - Parameters:
+  ///   - segments: an array of ``Segment`` instances defining the segmentation of the modified view.
+  ///   - verticalOffset: a vertical offset of content provided by a modified view. You can use it to animate your content.
+  /// - Returns: A view with applied effect.
+  ///
+  /// The first segment to be rendered gets constructed from coordinates of the top-right and top-left corners of the view and the `bottomEdge` of the first ``Segment`` in the `segments` array. Each next segment gets rendered in relation to the previous one, so the `bottomEdge` of the previous segment becomes a top edge of the next one. The final segment gets rendered using the last segment's `bottomEdge` as a top edge and coordinates of the bottom-left and bottom-right corners of the view.
+  ///
+  /// If the `segments` array is empty, the geometry of modified view gets rendered unchanged.
+  /// 
+  /// Segments are rendered in a top-to-bottom order, so if you have any overlaping segments the one with the lower index in the `segments` array will occlude the one with the higher index.
   public func segmentationEffect(
     segments: [Segment],
     verticalOffset: Float = 0
@@ -48,3 +31,43 @@ extension View {
       )
   }
 }
+
+/// `Segment` models a segment of rendered content.
+public struct Segment {
+  /// Defines the bottom edge of the segment.
+  ///
+  /// Segments are always rendered "under" an existing segment or edge (see the discussion section under ``SwiftUI/View/segmentationEffect(segments:verticalOffset:)``), so the `bottomEdge` is the only thing needed to define the geometry of a segment.
+  public var bottomEdge: Edge
+  
+  /// This parameter decides how "expanded" or "stretched" is the content inside the segment.
+  public var contentScale: Float
+  
+  public init(bottomEdge: Edge, contentScale: Float) {
+    self.bottomEdge = bottomEdge
+    self.contentScale = contentScale
+  }
+  
+  var floatArray: [Float] {
+    bottomEdge.floatArray + [contentScale]
+  }
+}
+
+/// `Edge` models a place where two segments meet.
+///
+/// It defines vertices using `SIMD2<Float>` `x`, `y` values in SwiftUI coordinate space ((0,0) in the top-left corner).
+public enum Edge {
+  /// A `node` models a single-vertex edge.
+  case node(SIMD2<Float>)
+  /// A `bar` models an edge that consists of two vertices.  To model a twisted segment make sure that the `x` value of the first vertex is greater than the `x` value of the second one.
+  case bar(SIMD2<Float>, SIMD2<Float>)
+  
+  var floatArray: [Float] {
+    switch self {
+    case .node(let position):
+      return position.xy + position.xy
+    case .bar(let lPosition, let rPosition):
+      return lPosition.xy + rPosition.xy
+    }
+  }
+}
+
